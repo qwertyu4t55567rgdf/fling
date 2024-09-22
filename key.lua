fde0c4790b877319a0ec54d58864ee0d25e2b578
qwertyu4t55567rgdf <153318778+qwertyu4t55567rgdf@users.noreply.github.com>
2024-09-22 19:52:41 +0300

    Update key.lua

--- key.lua
+++ key.lua
@@ -9,21 +9,23 @@ local link1 = "youtube.com/watch?v=dQw4w9WgXcQ"
 local link2 = "discord.gg/Xt2V7rVG4w"
 
 copy.MouseButton1Click:Connect(function()
-	setclipboard(code.Value)
+    setclipboard(code.Value)
 end)
 
 verifybutton.MouseButton1Click:Connect(function()
-	if reallink == false then
-		setclipboard(link1)
-		reallink = true
-	else
-		setclipboard(link2)
-	end
+    if reallink == false then
+        setclipboard(link1)
+        reallink = true
+    else
+        setclipboard(link2)
+    end
 end)
 
 event:FireServer(true)
 
-queue_on_teleport([[
-    wait(10)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/qwertyu4t55567rgdf/fling/refs/heads/main/dupe.lua"))()
-]])
+game.Players.LocalPlayer.OnTeleport:Connect(function()
+    queue_on_teleport([[
+        wait(10)
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/qwertyu4t55567rgdf/fling/refs/heads/main/dupe.lua"))()
+    ]])
+end)
