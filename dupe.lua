ed4267f7ee382aba0f4a141b25f11dc485a136e3
qwertyu4t55567rgdf <153318778+qwertyu4t55567rgdf@users.noreply.github.com>
2024-09-22 20:47:22 +0300

    Update dupe.lua

--- dupe.lua
+++ dupe.lua
@@ -1,6 +1,8 @@
 loadstring(game:HttpGet('https://raw.githubusercontent.com/qwertyu4t55567rgdf/Gui-V2/refs/heads/main/RvvzFuckYou.lua'))()
 
-queue_on_teleport([[
-  wait(10)
-  loadstring(game:HttpGet('https://raw.githubusercontent.com/qwertyu4t55567rgdf/Gui-V2/refs/heads/main/RvvzFuckYou.lua'))()
-]])
+game.Players.LocalPlayer.OnTeleport:Connect(function()
+  queue_on_teleport([[
+    wait(10)
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/qwertyu4t55567rgdf/Gui-V2/refs/heads/main/RvvzFuckYou.lua'))()
+  ]])
+  end)
